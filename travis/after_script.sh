34c908515527b21a4e0c684384ed6c3f1d564f8e
Jay Lyerly <jlyerly@stepleaderdigital.com>
2014-01-19 13:05:45 -0500

    Tweak coveralls settings.

--- travis/after_script.sh
+++ travis/after_script.sh
@@ -4,4 +4,5 @@ set -x
 
 GCOV_SRC_DIR=`xcodebuild -project Followers.xcodeproj -scheme Followers -showBuildSettings |grep -i " BUILD_DIR = " |awk -F= '{print $2}' |sed 's/ //g'`/..
 echo GCOV_SRC_DIR = $GCOV_SRC_DIR
-coveralls -b $GCOV_SRC_DIR --verbose -r . -x .m,.h,.mm
+coveralls -b $GCOV_SRC_DIR --verbose -r . -x .m -x .h -x .mm -e SA_OAuth -e MGTwitter -e OAuthConsumer -e Followers\ Tests/
+
