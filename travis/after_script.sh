c8a5a4b9c23cab0dd2858af748ffc95643cd74ef
Jay Lyerly <jlyerly@stepleaderdigital.com>
2014-01-19 11:30:39 -0500

    Fix extension list for code coverage

--- travis/after_script.sh
+++ travis/after_script.sh
@@ -4,4 +4,4 @@ set -x
 
 GCOV_SRC_DIR=`xcodebuild -project Followers.xcodeproj -scheme Followers -showBuildSettings |grep -i " BUILD_DIR = " |awk -F= '{print $2}' |sed 's/ //g'`/..
 echo GCOV_SRC_DIR = $GCOV_SRC_DIR
-coveralls -b $GCOV_SRC_DIR --verbose -r .
+coveralls -b $GCOV_SRC_DIR --verbose -r . -x .m,.h,.mm
