ce1f436ec8a4080ca04620b7e51435f4f6dc6403
Jay Lyerly <jlyerly@stepleaderdigital.com>
2014-01-19 14:28:42 -0500

    Coveralls debug

--- travis/after_script.sh
+++ travis/after_script.sh
@@ -4,5 +4,12 @@ set -x
 
 GCOV_SRC_DIR=`xcodebuild -project Followers.xcodeproj -scheme Followers -showBuildSettings |grep -i " BUILD_DIR = " |awk -F= '{print $2}' |sed 's/ //g'`/..
 echo GCOV_SRC_DIR = $GCOV_SRC_DIR
+
+echo Find GCDA
+find $GCOV_SRC_DIR -name \*.gcda
+echo Find GCNO
+find $GCOV_SRC_DIR -name \*.gcno
+
+
 coveralls -b $GCOV_SRC_DIR --verbose -r . -x .m -x .h -x .mm -e SA_OAuth -e MGTwitter -e OAuthConsumer -e Followers\ Tests/
 
