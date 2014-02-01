746c562638651ccd3378715d40c58112b258aa61
Jay Lyerly <jlyerly@stepleaderdigital.com>
2014-02-01 09:41:18 -0500

    Refactor twitter client -- consolidate API communication

--- Followers/SBFTwitterManager.m
+++ Followers/SBFTwitterManager.m
@@ -12,6 +12,8 @@
 #import "SBFTwitterUser.h"
 #import "SBFAlertManager.h"
 
+static const NSString *twitterEndpoint = @"https://api.twitter.com/1.1";
+
 typedef void (^SBFTwitterRequestSuccess)(NSDictionary* returnDict);
 typedef void (^SBFTwitterRequestError)(NSHTTPURLResponse *urlResponse,  NSError *error);
 
@@ -129,9 +131,15 @@ typedef void (^SBFTwitterRequestError)(NSHTTPURLResponse *urlResponse,  NSError 
     }
 }
 
-- (void)fetchFollowersForUser:(NSString *)username cursor:(NSString *)cursor completionBlock:(SBFTwitterFriendBlock)completionBlock {
-    NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1.1/followers/list.json"];
+- (NSURL *)urlForPath:(NSString *)path {
+    NSString *urlString = [NSString stringWithFormat:@"%@/%@", twitterEndpoint, path];
+    return [NSURL URLWithString:urlString];
+}
 
+- (void)fetchFollowersForUser:(NSString *)username cursor:(NSString *)cursor completionBlock:(SBFTwitterFriendBlock)completionBlock {
+    //NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1.1/followers/list.json"];
+    NSURL *url = [self urlForPath:@"followers/list.json"];
+    
     NSDictionary *params = @{
                              @"screen_name"            : username,
                              @"skip_status"            : @"1",
@@ -158,233 +166,43 @@ typedef void (^SBFTwitterRequestError)(NSHTTPURLResponse *urlResponse,  NSError 
     
 }
 
-/*
-- (void)fetchFollowersForUser:(NSString *)username cursor:(NSString *)cursor completionBlock:(SBFTwitterFriendBlock)completionBlock {
-    if ([self userHasAccessToTwitter]) {
-        
-        ACAccountType *twitterAccountType = [self.accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
-        
-        [self.accountStore requestAccessToAccountsWithType:twitterAccountType
-                                                   options:NULL
-                                                completion:^(BOOL granted, NSError *error) {
-             if (granted) {
-                 NSArray *twitterAccounts = [self.accountStore accountsWithAccountType:twitterAccountType];
-                 NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1.1/followers/list.json"];
-                 NSDictionary *params = @{
-                                              @"screen_name"            : username,
-                                              @"skip_status"            : @"1",
-                                              @"count"                  : @"50",
-                                              @"include_user_entities"  : @"false",
-                                              @"cursor"                 : cursor ?: @"-1",   // if cursor is nil, default to -1
-                                        };
-                 
-                 SLRequest *request = [SLRequest requestForServiceType:SLServiceTypeTwitter
-                                                         requestMethod:SLRequestMethodGET
-                                                                   URL:url
-                                                            parameters:params];
-                 
-                 //  Attach an account to the request
-                 [request setAccount:[twitterAccounts lastObject]];
-                 
-                 [request performRequestWithHandler: ^(NSData *responseData, NSHTTPURLResponse *urlResponse,  NSError *error) {
-                      if (responseData) {
-                          if (urlResponse.statusCode >= 200 && urlResponse.statusCode < 300) {
-                              NSError *jsonError;
-                              NSDictionary *returnDict =  [NSJSONSerialization JSONObjectWithData:responseData
-                                                                                            options:NSJSONReadingAllowFragments
-                                                                                              error:&jsonError];
-                              if (returnDict) {
-                                  NSArray *users = returnDict[@"users"] ?: @{};
-                                  NSString *cursorString = returnDict[@"next_cursor_str"];
-                                  
-                                  NSMutableArray *twUsers = [NSMutableArray array];
-                                  for (NSDictionary *userDict in users){
-                                      SBFTwitterUser *tu = [[SBFTwitterUser alloc] initWithDictionary:userDict];
-                                      [twUsers addObject:tu];
-                                  }
-                                  completionBlock([NSArray arrayWithArray:twUsers], cursorString);
-                              }
-                              else {
-                                  // Our JSON deserialization went awry
-                                  NSLog(@"JSON Error: %@", [jsonError localizedDescription]);
-                              }
-                          }
-                          else {
-                              // The server did not respond ... were we rate-limited?
-                              NSLog(@"The response status code is %d",
-                                    urlResponse.statusCode);
-                              NSDictionary *headers = [urlResponse allHeaderFields];
-                              NSLog(@"headers: %@", headers);
-                              if (urlResponse.statusCode == 429){
-                                  NSInteger reset = [(NSString *)headers[@"x-rate-limit-reset"] integerValue];
-                                  NSDate *resetDate = [NSDate dateWithTimeIntervalSince1970:reset];
-                                  NSLog(@"Current time is %@", [NSDate date]);
-                                  NSLog(@"Rate limit reset at %@", resetDate);
-                                  NSInteger minutes = floor([resetDate timeIntervalSinceNow] / 60);
-                                  NSInteger seconds = ((int)[resetDate timeIntervalSinceNow]) % 60;
-                                  NSLog(@"Rate limit reset in %02d:%02d ", minutes, seconds);
-                                  NSString *msg = [NSString stringWithFormat:@"Rate limit reset in\n%02d:%02d minutes", minutes, seconds];
-                                  NSString *title = @"Uh-oh!  Twitter API rate limit exceeded";
-                                  [[SBFAlertManager sharedManager] displayAlertTitle:title message:msg];
-                              }
-                              
-                          }
-                      }
-                  }];
-             }
-             else {
-                 // Access was not granted, or an error occurred
-                 NSLog(@"%@", [error localizedDescription]);
-             }
-         }];
-    } else {
-        // No access to twitter -- ie, no account
-        [[SBFAlertManager sharedManager] displayAlertTitle:@"No Twitter Access"
-                                                   message:@"Did you configure your Twitter accounts in the system settings?"];
-    }
-}
-*/
+
+- (void)fetchTimelineForUser:(NSString *)username {
+    NSURL *url = [self urlForPath:@"statuses/user_timeline.json"];
     
-- (void)fetchTimelineForUser:(NSString *)username
-{
-    //  Step 0: Check that the user has local Twitter accounts
-    if ([self userHasAccessToTwitter]) {
-        
-        //  Step 1:  Obtain access to the user's Twitter accounts
-        ACAccountType *twitterAccountType =
-        [self.accountStore accountTypeWithAccountTypeIdentifier:
-         ACAccountTypeIdentifierTwitter];
-        
-        [self.accountStore
-         requestAccessToAccountsWithType:twitterAccountType
-         options:NULL
-         completion:^(BOOL granted, NSError *error) {
-             if (granted) {
-                 //  Step 2:  Create a request
-                 NSArray *twitterAccounts =
-                 [self.accountStore accountsWithAccountType:twitterAccountType];
-                 NSURL *url = [NSURL URLWithString:@"https://api.twitter.com"
-                               @"/1.1/statuses/user_timeline.json"];
-                 NSDictionary *params = @{@"screen_name" : username,
-                                          @"include_rts" : @"0",
-                                          @"trim_user" : @"1",
-                                          @"count" : @"1"};
-                 SLRequest *request =
-                 [SLRequest requestForServiceType:SLServiceTypeTwitter
-                                    requestMethod:SLRequestMethodGET
-                                              URL:url
-                                       parameters:params];
-                 
-                 //  Attach an account to the request
-                 [request setAccount:[twitterAccounts lastObject]];
-                 
-                 //  Step 3:  Execute the request
-                 [request performRequestWithHandler:
-                  ^(NSData *responseData,
-                    NSHTTPURLResponse *urlResponse,
-                    NSError *error) {
-                      
-                      if (responseData) {
-                          if (urlResponse.statusCode >= 200 &&
-                              urlResponse.statusCode < 300) {
-                              
-                              NSError *jsonError;
-                              NSDictionary *timelineData =
-                              [NSJSONSerialization
-                               JSONObjectWithData:responseData
-                               options:NSJSONReadingAllowFragments error:&jsonError];
-                              if (timelineData) {
-                                  NSLog(@"Timeline Response: %@\n", timelineData);
-                              }
-                              else {
-                                  // Our JSON deserialization went awry
-                                  NSLog(@"JSON Error: %@", [jsonError localizedDescription]);
-                              }
-                          }
-                          else {
-                              // The server did not respond ... were we rate-limited?
-                              NSLog(@"The response status code is %d",
-                                    urlResponse.statusCode);
-                          }
-                      }
-                  }];
-             }
-             else {
-                 // Access was not granted, or an error occurred
-                 NSLog(@"%@", [error localizedDescription]);
-             }
-         }];
-    }
+    NSDictionary *params = @{
+                             @"screen_name" : username,
+                             @"include_rts" : @"0",
+                             @"trim_user"   : @"1",
+                             @"count"       : @"1",
+                             };
+    
+    SBFTwitterRequestSuccess successBlock = ^(NSDictionary* returnDict) {
+        NSLog(@"Timeline Response: %@\n", returnDict);
+    };
+    
+    [self twitterRequest:url params:params onSuccess:successBlock onError:nil];
+    
 }
 
-- (void)fetchInfoForUser:(NSString *)username completionBlock:(SBFTwitterInfoBlock)completionBlock
-{
-    //  Step 0: Check that the user has local Twitter accounts
-    if ([self userHasAccessToTwitter]) {
-        
-        //  Step 1:  Obtain access to the user's Twitter accounts
-        ACAccountType *twitterAccountType =
-        [self.accountStore accountTypeWithAccountTypeIdentifier:
-         ACAccountTypeIdentifierTwitter];
-        
-        [self.accountStore
-         requestAccessToAccountsWithType:twitterAccountType
-         options:NULL
-         completion:^(BOOL granted, NSError *error) {
-             if (granted) {
-                 //  Step 2:  Create a request
-                 NSArray *twitterAccounts =
-                 [self.accountStore accountsWithAccountType:twitterAccountType];
-                 NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1.1/users/show.json"];
-                 NSDictionary *params = @{ @"screen_name" : username };
-                 SLRequest *request = [SLRequest requestForServiceType:SLServiceTypeTwitter
-                                                         requestMethod:SLRequestMethodGET
-                                                                   URL:url
-                                                            parameters:params];
-                 
-                 //  Attach an account to the request
-                 [request setAccount:[twitterAccounts lastObject]];
-                 
-                 //  Step 3:  Execute the request
-                 [request performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
-                      if (responseData) {
-                          if (urlResponse.statusCode >= 200 && urlResponse.statusCode < 300) {
-                              NSError *jsonError;
-                              NSDictionary *userData = [NSJSONSerialization JSONObjectWithData:responseData
-                                                                                           options:NSJSONReadingAllowFragments
-                                                                                             error:&jsonError];
-                              if (userData) {
-                                  SBFTwitterUser *user = [[SBFTwitterUser alloc] initWithDictionary:userData];
-                                  completionBlock(user);
-                              }
-                              else {
-                                  // Our JSON deserialization went awry
-                                  NSLog(@"JSON Error: %@", [jsonError localizedDescription]);
-                              }
-                          }
-                          else {
-                              // The server did not respond ... were we rate-limited?
-                              NSLog(@"The response status code is %d",
-                                    urlResponse.statusCode);
-                          }
-                      }
-                  }];
-             }
-             else {
-                 // Access was not granted, or an error occurred
-                 NSLog(@"%@", [error localizedDescription]);
-             }
-         }];
-    }
+- (void)fetchInfoForUser:(NSString *)username completionBlock:(SBFTwitterInfoBlock)completionBlock {
+    NSURL *url = [self urlForPath:@"users/show.json"];
+    
+    NSDictionary *params = @{ @"screen_name" : username };
+    
+    SBFTwitterRequestSuccess successBlock = ^(NSDictionary* returnDict) {
+        SBFTwitterUser *user = [[SBFTwitterUser alloc] initWithDictionary:returnDict];
+        completionBlock(user);
+    };
+    
+    [self twitterRequest:url params:params onSuccess:successBlock onError:nil];
+    
 }
-
 
 - (NSString *)defaultUsername {
     ACAccountType *twitterAccountType = [self.accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
     ACAccount *twAccount = [[self.accountStore accountsWithAccountType:twitterAccountType] lastObject];
-    NSString *name = [NSString stringWithFormat:@"@%@", twAccount.username];   // prepend the @
-
-    return name;
+    return twAccount.username;
 }
 @end
 
